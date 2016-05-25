8d499d12a6769ea8dd6523416ba6856276c39638
mdorf <mdorf@stanford.edu>
2016-05-25 16:50:10 -0700

    implemented NCBO-1635 - Add a SPAM deletion job to ncbo_cron

--- lib/ncbo_cron/spam_deletion.rb
+++ lib/ncbo_cron/spam_deletion.rb
@@ -1,5 +1,4 @@
 require 'logger'
-require 'benchmark'
 require 'base64'
 
 module NcboCron
@@ -27,15 +26,9 @@ module NcboCron
 
       def run
         auth_token = Base64.decode64(NcboCron.settings.git_repo_access_token)
-        @logger.info("Deleting SPAM..."); @logger.flush
-        @logger.flush
-
-        time = Benchmark.realtime do
-          res = `curl --header 'Authorization: token #{auth_token}' --header 'Accept: application/vnd.github.v3.raw' --location #{FULL_FILE_PATH}`
-          usernames = res.split(",").map(&:strip)
-          delete_spam(usernames)
-        end
-        @logger.info("Finished deleting SPAM in #{time} sec."); @logger.flush
+        res = `curl --header 'Authorization: token #{auth_token}' --header 'Accept: application/vnd.github.v3.raw' --location #{FULL_FILE_PATH}`
+        usernames = res.split(",").map(&:strip)
+        delete_spam(usernames)
       end
 
       private
@@ -60,10 +53,7 @@ module NcboCron
           delete_users << user
         end
 
-        if (delete_users.length == 0 &&
-            delete_projects.length == 0 &&
-            delete_notes.length == 0 &&
-            delete_reviews.length == 0)
+        if delete_users.length == 0 && delete_projects.length == 0 && delete_notes.length == 0 && delete_reviews.length == 0
           @logger.info("No users/projects/notes/reviews found")
         else
           @logger.info("Deleting #{delete_projects.length} projects")
@@ -90,3 +80,4 @@ end
 # spam_deletion_path = File.join("logs", "spam-deletion.log")
 # spam_deletion_logger = Logger.new(spam_deletion_path)
 # NcboCron::Models::SpamDeletion.new(spam_deletion_logger).run
+# ./bin/ncbo_cron --disable-processing true --disable-pull true --disable-flush true --disable-warmq true --disable-ontology-analytics true --disable-mapping-counts true --disable-ontologies-report true --spam-deletion '14 * * * *'
