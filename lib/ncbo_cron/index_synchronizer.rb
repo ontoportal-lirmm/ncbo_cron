db38eff971fa25ffed46f3019967865fd3b3b5c5
mdorf <mdorf@stanford.edu>
2020-08-31 14:57:28 -0700

    fixes to #34 - index synchronizer

--- lib/ncbo_cron/index_synchronizer.rb
+++ lib/ncbo_cron/index_synchronizer.rb
@@ -19,7 +19,7 @@ module NcboCron
       # ALL classes are to be tested.
       MIN_PAGED_THRESHOLD = 1000
       # Maximum number of classes per page
-      MAX_PAGE_SIZE = 2000
+      MAX_PAGE_SIZE = 4000
       # Minimum number of days elapsed from the previous test
       # that warrant a re-test of a specific ontology
       ONTOLOGY_RESYNC_NUM_DAYS_MIN = 15
@@ -51,125 +51,141 @@ module NcboCron
       end
 
       def run_synchronizer(acronyms = [])
-        # acronyms = ['SO', 'NBO', 'ABD', 'CHEAR']
-        # acronyms = ['CHEAR', 'ABD']
-        ont_to_index = []
+        # acronyms = ['GWAS_EFO_SKOS', 'NBO', 'CHEAR']
+        ont_to_index = nil
 
         time = Benchmark.realtime do
           ont_to_index = ontologies_to_index(acronyms)
         end
-        output = "Completed the index synchronization run in #{(time / 60).round(1)} minutes."
+        @logger << "\n"
+        @logger.info "Completed the index synchronization run in #{(time / 60).round(1)} minutes."
 
-        if ont_to_index.empty?
-          output << ' ALL ontologies are in sync with the search index.'
+        if ont_to_index[:queued].empty?
+          @logger.info "No ontologies queued for re-indexing."
         else
-          output << " Ontologies queued for re-indexing:\n"
-          output << ont_to_index.join(', ')
+          @logger.info "Ontologies queued for re-indexing:\n#{ont_to_index[:queued].join(', ')}"
         end
-        @logger.info output
+
+        unless ont_to_index[:unqueued].empty?
+          @logger.info "Ontologies that require further investigation:\n#{ont_to_index[:unqueued].join(', ')}"
+        end
+        @logger << "\n\n"
       end
 
       def ontologies_to_index(acronyms = [])
-        ont_to_index = []
+        ont_to_index = { queued: [], unqueued: [] }
         redis = Redis.new(host: NcboCron.settings.redis_host, port: NcboCron.settings.redis_port)
         ontologies = LinkedData::Models::Ontology.where.include(:acronym, :submissionId).all
         ontologies.select! { |ont| acronyms.include?(ont.acronym) } unless acronyms.empty?
+        remaining_ontologies = ontologies.length
+        @logger.info("There is a total of #{remaining_ontologies} ontolog#{remaining_ontologies > 1 ? "ies" : "y"} to process")
 
         ontologies.each do |ont|
-          submission = ont.latest_submission(status: [:RDF])
-          next if submission.nil?
+          begin
+            acronym = ont.acronym
+            @logger << "\n"
+            @logger.info "Processing ontology #{acronym}"
+            remaining_ontologies -= 1
+            @logger.info "#{remaining_ontologies} out of #{ontologies.length} ontologies remaining" if remaining_ontologies > 0
+            submission = ont.latest_submission(status: [:RDF])
 
-          acronym = ont.acronym
-          submission.bring(:submissionId) if submission.bring?(:submissionId)
-          current_submission_id = submission.submissionId
-          synced_submission_id = current_submission_id
-          registry_data = redis.hgetall("#{REDIS_PREFIX}#{acronym}")
-          # Choosing re-sync number of days elapsed at random between MIN and MAX to avoid
-          # the majority of the ontologies to be due for a re-sync on a single given day.
-          # Choosing this number at random (in range) allows for staggered re-syncs
-          resync_num_days = rand(ONTOLOGY_RESYNC_NUM_DAYS_MIN..ONTOLOGY_RESYNC_NUM_DAYS_MAX)
-          last_synced_date = DateTime.now - resync_num_days - 1
-          is_synced = false
-          num_failures = 0
-          msg_synced = ''
-
-          unless registry_data.empty?
-            is_synced = registry_data['is_synced'] == "true"
-            num_failures = registry_data['num_failures'].to_i
-            msg_synced = " The ontology was not synced at last run. Failed #{num_failures} runs." unless is_synced
-            synced_submission_id = registry_data['submission_id'].to_i
-            last_synced_date = DateTime.parse(registry_data['last_synced_date'])
-          end
-          time_now = DateTime.now
-          last_synced_diff = (time_now - last_synced_date).to_i
-          @logger.info "Processing ontology #{acronym}"
-          no_resync = is_synced && current_submission_id == synced_submission_id && last_synced_diff < resync_num_days
-          msg = "Last sync #{last_synced_diff} days ago. Re-sync required number of days: #{resync_num_days}."
-          msg << ' Detected a new submission ID.' unless current_submission_id == synced_submission_id
-          msg << (no_resync ? " No re-sync required. Skipping...\n\n" : "#{msg_synced} Proceeding with a re-sync.\n\n")
-          @logger.info msg
-          next if no_resync
-
-          last_synced_date = time_now
-          page_data = LinkedData::Models::Class.in(submission).page(1, 1).all
-          total_count = page_data.aggregate
-          total_test_count = total_count
-          test_page_size = total_count
-          test_page_count = 1
-          total_page_count = 1
-          test_page_numbers = [1]
-
-          if total_count > MIN_PAGED_THRESHOLD
-            total_test_count = (total_count * PERCENT_CLASSES_TO_TEST / 100).round
-            test_page_size = (total_test_count * PERCENT_TEST_PAGE_SIZE / 100).round
-            test_page_size = MAX_PAGE_SIZE if test_page_size > MAX_PAGE_SIZE
-            test_page_count = (total_test_count / test_page_size.to_f).ceil
-            total_page_count = (total_count / test_page_size.to_f).ceil
-            test_page_numbers = (1..total_page_count).to_a.sort { rand - 0.5 }[1..test_page_count].sort
-          end
-          @logger.info "Total Classes: #{total_count}"
-          @logger.info "Total Test Classes: #{total_test_count}"
-          @logger.info "Test Page Size: #{test_page_size}"
-          @logger.info "Total Number of Pages: #{total_page_count}"
-          @logger.info "Test Number of Pages: #{test_page_count}"
-          @logger.info "Test Page Numbers: #{test_page_numbers}\n"
-
-          test_page_numbers.each do |page_num|
-            page_data = LinkedData::Models::Class.in(submission).page(page_num, test_page_size).all
-            resource_ids = page_data.map { |cl| cl.id.to_s }
-            search_query = search_query_params(ont.acronym, test_page_size, resource_ids)
-            search_resp = LinkedData::Models::Class.search('*', search_query)
-            total_found = search_resp['response']['numFound']
-            # @logger.info "Page: #{page_num}"
-            # @logger.info "Number of classes: #{resource_ids.length}"
-            # @logger.info "Num found: #{total_found}"
-            # @logger.info "*********************\n"
-
-            if total_found < resource_ids.length
-              found_ids = search_resp['response']['docs'].map { |cl| cl['resource_id'] }
-              @logger.info "Page: #{page_num}"
-              @logger.info "Number of classes: #{resource_ids.length}"
-              @logger.info "Num found: #{total_found}"
-              @logger.info "*********************\n"
-              ont_to_index << acronym
-              is_synced = false
-              num_failures += 1
-
-              if num_failures > MAX_FAILED_RETRIES
-                @logger.error "********************************************************************"
-                @logger.error "Ontology #{acronym} failed to be synchronized #{num_failures} times. Please troubleshoot manually."
-                @logger.error "********************************************************************"
-              else
-                NcboCron::Models::OntologySubmissionParser.new.queue_submission(submission, { index_search: true })
-                @logger.info "Ontology #{acronym} is missing classes from the index. Queued for re-indexing."
-                @logger.info "Classes missing from the index: #{(resource_ids - found_ids)}"
-              end
-              break
-            else
-              is_synced = true
+            if submission.nil?
+              @logger.info "Ontology #{acronym} does not contain a parsed submission. Skipping check..."
+              next
             end
+            submission.bring(:submissionId) if submission.bring?(:submissionId)
+            current_submission_id = submission.submissionId
+            synced_submission_id = current_submission_id
+            registry_data = redis.hgetall("#{REDIS_PREFIX}#{acronym}")
+            # Choosing re-sync number of days elapsed at random between MIN and MAX to avoid
+            # the majority of the ontologies to be due for a re-sync on a single given day.
+            # Choosing this number at random (in range) allows for staggered re-syncs
+            resync_num_days = rand(ONTOLOGY_RESYNC_NUM_DAYS_MIN..ONTOLOGY_RESYNC_NUM_DAYS_MAX)
+            last_synced_date = DateTime.now - resync_num_days - 1
+            is_synced = false
+            num_failures = 0
+            msg_synced = ''
+
+            unless registry_data.empty?
+              is_synced = registry_data['is_synced'] == "true"
+              num_failures = registry_data['num_failures'].to_i
+              msg_synced = " The ontology was not synced at last run. Failed #{num_failures} runs." unless is_synced
+              synced_submission_id = registry_data['submission_id'].to_i
+              last_synced_date = DateTime.parse(registry_data['last_synced_date'])
+            end
+            time_now = DateTime.now
+            last_synced_diff = (time_now - last_synced_date).to_i
+            no_resync = is_synced && current_submission_id == synced_submission_id && last_synced_diff < resync_num_days
+            msg = "Last sync #{last_synced_diff} days ago. Re-sync required days: #{resync_num_days}."
+            msg << ' Detected a new submission ID.' unless current_submission_id == synced_submission_id
+            msg << (no_resync ? " No re-sync required. Skipping check..." : "#{msg_synced}" << (num_failures > MAX_FAILED_RETRIES ? " Proceeding with a re-sync.\n\n" : ''))
+            @logger.info msg
+            next if no_resync
+
+            last_synced_date = time_now
+            page_data = LinkedData::Models::Class.in(submission).page(1, 1).all
+            total_count = page_data.aggregate
+            total_test_count = total_count
+            test_page_size = total_count
+            test_page_count = 1
+            total_page_count = 1
+            test_page_numbers = [1]
+
+            if total_count > MIN_PAGED_THRESHOLD
+              total_test_count = (total_count * PERCENT_CLASSES_TO_TEST / 100).round
+              test_page_size = (total_test_count * PERCENT_TEST_PAGE_SIZE / 100).round
+              test_page_size = MAX_PAGE_SIZE if test_page_size > MAX_PAGE_SIZE
+              test_page_count = (total_test_count / test_page_size.to_f).ceil
+              total_page_count = (total_count / test_page_size.to_f).ceil
+              test_page_numbers = (1..total_page_count).to_a.sort { rand - 0.5 }[1..test_page_count].sort
+            end
+            @logger.info "Total Classes: #{total_count}"
+            @logger.info "Total Test Classes: #{total_test_count}"
+            @logger.info "Test Page Size: #{test_page_size}"
+            @logger.info "Total Number of Pages: #{total_page_count}"
+            @logger.info "Test Number of Pages: #{test_page_count}"
+            @logger.info "Test Page Numbers: #{test_page_numbers}"
+            ind = 0
+
+            test_page_numbers.each do |page_num|
+              ind += 1
+              page_data = LinkedData::Models::Class.in(submission).page(page_num, test_page_size).all
+              resource_ids = page_data.map { |cl| cl.id.to_s }
+              search_query = search_query_params(ont.acronym, test_page_size, resource_ids)
+              search_resp = LinkedData::Models::Class.search('*', search_query)
+              total_found = search_resp['response']['numFound']
+              @logger.info "Page #{ind} (#{page_num}) out of #{test_page_count} with #{resource_ids.length} classes"
+
+              if total_found < resource_ids.length
+                found_ids = search_resp['response']['docs'].map { |cl| cl['resource_id'] }
+                @logger.info "Classes found in index: #{total_found}"
+                @logger.info "*********************"
+                is_synced = false
+                num_failures += 1
+
+                if num_failures > MAX_FAILED_RETRIES
+                  ont_to_index[:unqueued] << acronym
+                  num_failures -= 1
+                  @logger.error "*****************************************************************************"
+                  @logger.error "Ontology #{acronym} failed to be synchronized #{num_failures} times. Please troubleshoot manually."
+                  @logger.error "*****************************************************************************"
+                else
+                  ont_to_index[:queued] << acronym
+                  # NcboCron::Models::OntologySubmissionParser.new.queue_submission(submission, { index_search: true })
+                  @logger.info "Ontology #{acronym} is missing classes from the index. Queued for re-indexing."
+                  @logger.info "Classes missing from the index: #{(resource_ids - found_ids)}"
+                end
+                break
+              else
+                is_synced = true
+              end
+            end
+            @logger.info "No discrepancies discovered with ontology #{acronym}" if is_synced
+            redis.hmset("#{REDIS_PREFIX}#{acronym}", "submission_id", current_submission_id, "is_synced", is_synced, "num_failures", num_failures, "last_synced_date", last_synced_date)
+          rescue RuntimeError => e
+            msg = "Error processing ontology #{acronym}: #{e.class}: #{e.message}\n#{e.backtrace.join("\n\t")}"
+            @logger.error(msg)
           end
-          redis.hmset("#{REDIS_PREFIX}#{acronym}", "submission_id", current_submission_id, "is_synced", is_synced, "num_failures", num_failures, "last_synced_date", last_synced_date)
         end
         ont_to_index
       end
@@ -213,7 +229,9 @@ end
 # require 'ncbo_cron/config'
 # require_relative '../../config/config'
 #
-# NcboCron::Models::IndexSynchronizer.new(Logger.new(STDOUT)).run
+# index_synchronizer_path = File.join("logs", "index-synchronizer.log")
+# index_synchronizer_logger = Logger.new(index_synchronizer_path)
+# NcboCron::Models::IndexSynchronizer.new(index_synchronizer_logger).run
 # ./bin/ncbo_cron --disable-processing true --disable-pull true --disable-flush true --disable-warmq true --disable-ontology-analytics true --disable-mapping-counts true --disable-spam-deletion true --disable-ontologies-report true --index-synchronizer '14 * * * *'
 
 
