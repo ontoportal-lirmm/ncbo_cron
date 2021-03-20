bed82e7eac75184c72ec2c7acd3c4fe51506d066
mdorf <mdorf@stanford.edu>
2021-03-19 17:48:39 -0700

    implemented a script to allow regeneration of mapping counts on demand

--- lib/ncbo_cron/mapping_counts.rb
+++ lib/ncbo_cron/mapping_counts.rb
@@ -14,3 +14,15 @@ module NcboCron
   
   end
 end
+
+# require 'ontologies_linked_data'
+# require 'goo'
+# require 'ncbo_annotator'
+# require 'ncbo_cron/config'
+# require_relative '../../config/config'
+#
+# log_path = File.join("logs", "mapping-counts.log")
+# logger = Logger.new(log_path)
+# NcboCron::Models::MappingCounts.new(logger).run
+
+# bundle exec ruby ./lib/ncbo_cron/mapping_counts.rb
