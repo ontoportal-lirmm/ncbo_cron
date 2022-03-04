364ae53586543f71704e92f82acb96e8560a4877
Alex Skrenchuk <alexskr@stanford.edu>
2022-03-04 02:30:58 -0800

    Revert "add docker support for unit testing"
    This reverts commit cc5cc6598c225e4d10e5bbf37e5765218573ad49.

--- test/test_case.rb
+++ test/test_case.rb
@@ -1,62 +1,29 @@
-require 'ontologies_linked_data'
-require_relative '../lib/ncbo_cron'
-#require_relative '../config/config'
+require "ontologies_linked_data"
+require_relative "../lib/ncbo_cron"
+require_relative "../config/config.rb"
 
 Goo.use_cache = false # Make sure tests don't cache
 
-require 'test/unit'
-
-SOLR_HOST  = ENV.include?('SOLR_HOST')  ? ENV['SOLR_HOST']  : 'localhost'
-REDIS_HOST = ENV.include?('REDIS_HOST') ? ENV['REDIS_HOST'] : 'localhost'
-REDIS_PORT = ENV.include?('REDIS_PORT') ? ENV['REDIS_PORT'] : 6379
-
-LinkedData.config do |config|
-  config.goo_backend_name           = ENV.include?('GOO_BACKEND_NAME') ? ENV['GOO_BACKEND_NAME'] : 'localhost'
-  config.goo_port                   = ENV.include?('GOO_PORT')         ? ENV['GOO_PORT']         : 9000
-  config.goo_host                   = ENV.include?('GOO_HOST')         ? ENV['GOO_HOST']         : 'localhost'
-  config.goo_path_query             = ENV.include?('GOO_PATH_QUERY')   ? ENV['GOO_PATH_QUERY']   : '/sparql/'
-  config.goo_path_data              = ENV.include?('GOO_PATH_DATA')    ? ENV['GOO_PATH_DATA']    : '/data/'
-  config.goo_path_update            = ENV.include?('GOO_PATH_UPDATE')  ? ENV['GOO_PATH_UPDATE']  : '/update/'
-  config.goo_redis_port             = REDIS_PORT.to_i
-  config.goo_redis_host             = REDIS_HOST.to_s
-  config.http_redis_port            = REDIS_PORT.to_i
-  config.http_redis_host            = REDIS_HOST.to_s
-  config.search_server_url          = "http://#{SOLR_HOST}:8983/solr/term_search_core1"
-  config.property_search_server_url = "http://#{SOLR_HOST}:8983/solr/prop_search_core1"
-end
-Annotator.config do |config|
-  config.annotator_redis_host          = REDIS_HOST.to_s
-  config.annotator_redis_port          = REDIS_PORT.to_i
-  config.mgrep_host                    = ENV.include?('MGREP_HOST') ? ENV['MGREP_HOST'] : 'localhost'
-  config.mgrep_port                    = ENV.include?('MGREP_PORT') ? ENV['MGREP_PORT'] : 55555
-  config.mgrep_dictionary_file         = './test/data/dictionary.txt'
-end
-LinkedData::OntologiesAPI.config do |config|
-  config.http_redis_host = REDIS_HOST.to_s
-  config.http_redis_port = REDIS_PORT.to_i
-end
-
-NcboCron.config do |config|
-  config.redis_host = REDIS_HOST.to_s
-  config.redis_port = REDIS_PORT.to_i
-end
+require "test/unit"
 
 # Check to make sure you want to run if not pointed at localhost
-safe_host = Regexp.new(/localhost|-ut|ncbo-dev*|ncbo-unittest*/)
+safe_host = Regexp.new(/localhost|ncbo-dev*|ncbo-stg-app-22*|ncbo-unittest*/)
 unless LinkedData.settings.goo_host.match(safe_host) &&
-       LinkedData.settings.search_server_url.match(safe_host) &&
-       NcboCron.settings.redis_host.match(safe_host)
-  print '\n\n================================== WARNING ==================================\n'
-  print '** TESTS CAN BE DESTRUCTIVE -- YOU ARE POINTING TO A POTENTIAL PRODUCTION/STAGE SERVER **\n'
-  print 'Servers:\n'
+    LinkedData.settings.search_server_url.match(safe_host) &&
+    NcboCron.settings.redis_host.match(safe_host)
+  print "\n\n================================== WARNING ==================================\n"
+  print "** TESTS CAN BE DESTRUCTIVE -- YOU ARE POINTING TO A POTENTIAL PRODUCTION/STAGE SERVER **\n"
+  print "Servers:\n"
   print "triplestore -- #{LinkedData.settings.goo_host}\n"
   print "search -- #{LinkedData.settings.search_server_url}\n"
   print "redis -- #{NcboCron.settings.redis_host}\n"
   print "Type 'y' to continue: "
   $stdout.flush
   confirm = $stdin.gets
-  abort('Canceling tests...\n\n') unless confirm.strip == 'y'
-  print 'Running tests...'
+  if !(confirm.strip == 'y')
+    abort("Canceling tests...\n\n")
+  end
+  print "Running tests..."
   $stdout.flush
 end
 
@@ -79,15 +46,15 @@ class CronUnit < MiniTest::Unit
         query = "submissionAcronym:#{o.acronym}"
         LinkedData::Models::Ontology.unindexByQuery(query)
       end
-      LinkedData::Models::Ontology.indexCommit
-      Goo.sparql_update_client.update('DELETE {?s ?p ?o } WHERE { ?s ?p ?o }')
+      LinkedData::Models::Ontology.indexCommit()
+      Goo.sparql_update_client.update("DELETE {?s ?p ?o } WHERE { ?s ?p ?o }")
       LinkedData::Models::SubmissionStatus.init_enum
       LinkedData::Models::OntologyFormat.init_enum
       LinkedData::Models::OntologyType.init_enum
       LinkedData::Models::Users::Role.init_enum
       LinkedData::Models::Users::NotificationType.init_enum
     else
-      raise Exception, 'Too many triples in KB, does not seem right to run tests'
+      raise Exception, "Too many triples in KB, does not seem right to run tests"
     end
   end
 
@@ -100,24 +67,28 @@ class CronUnit < MiniTest::Unit
   end
 
   def _run_suites(suites, type)
-    before_suites
-    super(suites, type)
-  ensure
-    after_suites
+    begin
+      before_suites
+      super(suites, type)
+    ensure
+      after_suites
+    end
   end
 
   def _run_suite(suite, type)
-    backend_4s_delete
-    suite.before_suite if suite.respond_to?(:before_suite)
-    super(suite, type)
-  rescue Exception => e
-    puts e.message
-    puts e.backtrace.join("\n\t")
-    puts 'Traced from:'
-    raise e
-  ensure
-    backend_4s_delete
-    suite.after_suite if suite.respond_to?(:after_suite)
+    begin
+      backend_4s_delete
+      suite.before_suite if suite.respond_to?(:before_suite)
+      super(suite, type)
+    rescue Exception => e
+      puts e.message
+      puts e.backtrace.join("\n\t")
+      puts "Traced from:"
+      raise e
+    ensure
+      backend_4s_delete
+      suite.after_suite if suite.respond_to?(:after_suite)
+    end
   end
 end
 MiniTest::Unit.runner = CronUnit.new
