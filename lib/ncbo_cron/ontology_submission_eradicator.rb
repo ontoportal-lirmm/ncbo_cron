0f5de1b681593c7cd0f0fbcb559b17ab40b2870a
Syphax Bouazzouni <gs_bouazzouni@esi.dz>
2022-04-04 18:18:29 +0200

    not remove the submission directory beaucse it's already done by the submission.delete

--- lib/ncbo_cron/ontology_submission_eradicator.rb
+++ lib/ncbo_cron/ontology_submission_eradicator.rb
@@ -14,10 +14,9 @@ module NcboCron
       def initialize()
       end
 
-      def eradicate(ontology_acronym, submission , force=false)
+      def eradicate(submission , force=false)
         submission.bring(:submissionStatus) if submission.bring(:submissionStatus)
         if submission.archived? || force
-          delete_submission_files ontology_acronym, submission
           delete_submission_data submission
         else submission.ready?
           raise RemoveNotArchivedSubmissionException, "Submission #{submission.submissionId} is not an archived submission"
@@ -34,15 +33,6 @@ module NcboCron
         end
       end
 
-      def delete_submission_files(ontology_acronym, submission)
-        begin
-          # delete the folder
-          submission_dir = File.join(LinkedData.settings.repository_folder, ontology_acronym.to_s, submission.submissionId.to_s)
-          FileUtils.remove_dir(submission_dir, force: true)
-        rescue Exception => e
-          raise RemoveSubmissionFileException, e.message
-        end
-      end
 
     end
   end
